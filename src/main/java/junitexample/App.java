f5dc4f12408f7fd7f3632e6c961a3f48cf3b84c3
Alin Stefanescu <alinstef@users.noreply.github.com>
2026-04-21 11:36:25 +0300

    Fix find_min to return the minimum value

--- src/main/java/junitexample/App.java
+++ src/main/java/junitexample/App.java
@@ -14,7 +14,7 @@ public class App {
          int x, i;
          x = a[0];
          for (i = 1; i < a.length; i++) {
-           if (a[i] > x)
+           if (a[i] < x)
               x = a[i];
          }
          return x;
